5444685b79fc27691a63daccbdf44e3a4a5aee30
RexConner1 <oronadavid@gmail.com>
2025-12-20 14:55:03 -0600

    Local run

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+// import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), basicSsl()],
+    plugins: [react()],
     build: {
         outDir: "docs"
     },
     server: {
-        https: true,
+        https: false,
     },
     base: "/Monopoly/",
 });
