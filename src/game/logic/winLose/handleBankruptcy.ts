f99897be67b015118c8bf9c06dfb9831efc231e1
RexConner1 <oronadavid@gmail.com>
2026-04-07 18:01:46 -0500

    Win or lose functions

---
+++ src/game/logic/winLose/handleBankruptcy.ts
@@ -0,0 +1,47 @@
+import { GameContext } from "../../../assets/gameContext";
+import { PlayerJSON } from "../../../assets/player";
+import { showDialog } from "../../../ui/dialogs/dialogFactory";
+import { notifyMessage } from "../../../ui/notifications/notificationFactory";
+
+export function handlePlayerBankruptcy({
+    bankruptPlayer,
+    mainTheme,
+    ctx,
+    destroyPlayer
+}: {
+    bankruptPlayer: PlayerJSON;
+    mainTheme: HTMLAudioElement;
+    ctx: GameContext
+    destroyPlayer: (id: string) => void;
+}) {
+    if (bankruptPlayer.balance < 0) {
+        if (bankruptPlayer.id !== ctx.socket.id) {
+            if (ctx.clients.size > 2) {
+                const name = bankruptPlayer.username;
+                notifyMessage(ctx.notifyRef, "PLAYER_LOST", { name });
+            } else {
+                if (ctx.clients.has(ctx.socket.id)) {
+                    mainTheme.pause();
+                    showDialog(ctx.notifyRef, "YOU_WIN", {
+                        balance: ctx.clients.get(ctx.socket.id)?.balance
+                    });
+                } else {
+                    const xclient = Array.from(ctx.clients.values()).filter((v) => v.id !== bankruptPlayer.id)[0];
+                    const name = xclient.username ?? 0;
+                    mainTheme.pause();
+                    showDialog(ctx.notifyRef, "PLAYER_WON", {
+                        playerName: name,
+                        balance: ctx.clients.get(ctx.socket.id)?.balance
+                    });
+                }
+            }
+        } else {
+            mainTheme.pause();
+            showDialog(ctx.notifyRef, "YOU_LOST", {
+                balance: ctx.clients.get(ctx.socket.id)?.balance
+            }, "losing");
+        }
+
+        destroyPlayer(bankruptPlayer.id);
+    }
+}
---
+++ src/game/logic/winLose/handleMonopolsTrains.ts
@@ -0,0 +1,78 @@
+import { Property } from "../../../assets/property";
+import { showDialog } from "../../../ui/dialogs/dialogFactory";
+import monopolyJSON from "../../../assets/monopoly.json";
+import { GameContext } from "../../../assets/gameContext";
+
+export function handleMonopolsTrains({
+    winningMode,
+    mainTheme,
+    ctx
+}: {
+    winningMode: string;
+    mainTheme: HTMLAudioElement;
+    ctx: GameContext
+}) {
+    if (winningMode === "monopols" || winningMode === "monopols & trains") {
+        function removeDuplicates(originalList: Array<any>) {
+            // Create an empty array to store unique values
+            const uniqueList: Array<any> = [];
+
+            // Use the filter method to iterate through the original list
+            originalList.filter(function (item) {
+                // If the item is not already in the uniqueList, add it
+                if (!uniqueList.includes(item)) {
+                    uniqueList.push(item);
+                }
+                // Always return false in the filter function to skip duplicates
+                return false;
+            });
+
+            // Return the uniqueList
+            return uniqueList;
+        }
+        for (const p of Array.from(ctx.clients.values())) {
+            const prpGrups = [];
+            for (const prp of p.properties) {
+                if (!["Special", "Railroad", "Utilities"].includes(prp.group)) prpGrups.push(prp.group);
+            }
+            let x: number = 0;
+
+            for (const g of removeDuplicates(prpGrups)) {
+                const c = prpGrups.filter((v) => v === g).length;
+                const monopolyProperties = monopolyJSON.properties as Property[];
+                const cc = monopolyProperties.filter((v) => v.group === g).length;
+                if (c === cc) {
+                    x += 1;
+                }
+            }
+            if (x === 3) {
+                mainTheme.pause();
+                if (p.id === ctx.socket.id) {
+                    showDialog(ctx.notifyRef, "THREE_SETS");
+                } else {
+                    showDialog(ctx.notifyRef, "THREE_SETS", {
+                        playerName: p.username
+                    });
+                }
+                return;
+            }
+        }
+        if (winningMode === "monopols & trains") {
+            // continue with trains winning state!
+            for (const p of Array.from(ctx.clients.values())) {
+                const c = p.properties.filter((v) => v.group === "Railroad").length;
+                if (c === 4) {
+                    mainTheme.pause();
+                    if (p.id === ctx.socket.id) {
+                        showDialog(ctx.notifyRef, "FOUR_RAILROADS");
+                    } else {
+                        showDialog(ctx.notifyRef, "FOUR_RAILROADS", {
+                            playerName: p.username
+                        });
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
